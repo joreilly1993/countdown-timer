25b444ff7a145403bbd78d74a990d3eaa9d002af
John O'Reilly <johnoreilly.cny@gmail.com>
2022-03-15 20:44:36 -0400

    Timer duration input allows for a user to change the time remaining and countdown from that number. The start and pause button both work as expected.

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('test');
+class Timer {
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+
+        this.pauseButton.addEventListener('click',this.pause);
+    }
+    start = () => {
+        this.tick();
+        this.intervalID = setInterval(this.tick, 1000);
+    }
+    tick = () => {
+        if(this.timeRemaining <= 0) {
+            this.pause();
+        } else {
+         this.timeRemaining = this.timeRemaining -1;
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+
+    pause = () => {
+        clearInterval(this.intervalID);
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
